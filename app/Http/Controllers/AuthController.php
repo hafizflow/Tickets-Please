97d9515c098274316ec265665a7a68cefec24824
hafizflow <hafizur.rahman.cs@gmail.com>
2025-07-25 20:21:11 +0600

    Day 1 & 2

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ApiLoginRequest;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponses;
+
+class AuthController extends Controller
+{
+    use ApiResponses;
+    public function login(ApiLoginRequest $request){
+        return $this->ok($request->email);
+    }
+
+    public function register()
+    {
+        return $this->ok('Register');
+    }
+}
