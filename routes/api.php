39022fe45bac0de3002c7ac3b4620ea8dbaf1df2
hafizflow <hafizur.rahman.cs@gmail.com>
2025-07-25 21:09:08 +0600

    Day 3

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,9 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 
+Route::get('/tickets', function () {
+    return Ticket::all();
+});
 
 
 Route::get('/user', function (Request $request) {
